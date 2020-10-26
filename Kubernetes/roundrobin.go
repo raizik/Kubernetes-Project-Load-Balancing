dd9fbf4e6836c1464bbe701b3deaea72ff9085ec
raizik <raizik@campus.technion.ac.il>
2020-10-26 17:55:41 +0200

    update

--- Kubernetes/roundrobin.go
+++ Kubernetes/roundrobin.go
@@ -75,7 +75,7 @@ type balancerState struct {
 	affinity  affinityPolicy
 	busyPods map[string]string // busyPods[i] == j <=> endpoint ipport j is busy. init to empty
 	last_endpoint string      // last ipport pod the LB sent a job to
-	idleTokens int	// init to len(endpoints) number of idle pods in idlePods array (and number of ones in tickets)
+	idleTokens int	// number of idle tokens. init to number of endpoints 
 }
 
 func newAffinityPolicy(affinityType v1.ServiceAffinity, ttlSeconds int) *affinityPolicy {
